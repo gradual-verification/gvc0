6b7d6c9eede30392be3ba783b6ed8d6a7a956ca9
Conrad Zimmerman <me@conradz.com>
2021-10-30 20:38:30 -0400

    Add `--weave` to weave and print woven output

--- src/main/scala/gvc/main.scala
+++ src/main/scala/gvc/main.scala
@@ -9,6 +9,7 @@ import gvc.transformer._
 import viper.silicon.Silicon
 import viper.silver.verifier
 import viper.silver.verifier
+import gvc.weaver.Weaver
 
 object Main extends App {
 
@@ -31,10 +32,12 @@ object Main extends App {
   val files = ListBuffer[String]()
   var printC0 = false
   var printSilver = false
+  var printWeaving = false
 
   for (arg <- args) arg match {
     case "--c0" => printC0 = true
     case "--silver" => printSilver = true
+    case "--weave" => printWeaving = true
     case flag if flag.startsWith("--") => {
       println(s"Invalid flag '$flag'")
       sys.exit(1)
@@ -79,16 +82,11 @@ object Main extends App {
 
     val ir = Transformer.programToIR(resolved)
 
-    // TODO: Implement printer for whole program
-    val c0 = ir.methods.collect { case m: IR.MethodImplementation => m }
-      .map(CNaughtPrinter.printMethod(_))
-      .mkString("\n")
-
     val silver = SilverOutput.program(ir)
 
     if (printC0) {
       println(s"C0 output for '$name':")
-      println(c0)
+      println(CNaughtPrinter.printProgram(ir))
     }
 
     if (printSilver) {
@@ -99,7 +97,15 @@ object Main extends App {
     println(s"Verifying '$name'...")
 
     silicon.verify(silver) match {
-      case verifier.Success => println(s"Verified successfully!")
+      case verifier.Success => {
+        println(s"Verified successfully!")
+
+        if (printWeaving) {
+          val woven = Weaver.weave(ir, silver)
+          println(s"Woven output for '$name':")
+          println(CNaughtPrinter.printProgram(woven))
+        }
+      }
       case verifier.Failure(errors) => {
         println(s"Verification errors in '$name':")
         println(errors.map(_.readableMessage).mkString("\n"))
