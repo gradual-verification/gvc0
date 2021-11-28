e469bd828953af0543f08162452f388a227a31cc
Conrad Zimmerman <me@conradz.com>
2021-11-28 14:20:53 -0500

    Convert tests to IRGraph and add test auto-updater

--- src/test/scala/transformer/GraphTransformerTests.scala
+++ src/test/scala/transformer/GraphTransformerTests.scala
@@ -9,8 +9,12 @@ import gvc.transformer.GraphTransformer
 import gvc.transformer.GraphPrinter
 import org.scalatest.funsuite.AnyFunSuite
 import gvc.transformer.IRGraphSilver
+import java.io.File
+import java.io.PrintWriter
 
 class GraphTransformerTests extends AnyFunSuite {
+  val UPDATE = false
+
   val testFiles = List(
     "ir/assert_1",
     "ir/assert_2",
@@ -22,13 +26,48 @@ class GraphTransformerTests extends AnyFunSuite {
     "ir/struct_embed",
     "ir/ternary",
     "ir/while",
+    "viper/call",
+    "viper/char",
+    "viper/field",
+    "viper/imprecise_1",
+    "viper/imprecise_2",
+    "viper/imprecise_3",
+    "viper/int_pointer",
+    "viper/main",
+    "viper/negate",
+    "viper/not",
+    "viper/predicate_1",
+    "viper/specs",
+    "viper/while",
   )
+
+  def getFile(name: String): Option[String] = getClass().getResource("/" + name) match {
+    case null => None
+    case url => Some(Source.fromURL(url).mkString)
+  }
+
+  def assertFile(name: String, actual: String): Unit = {
+    getFile(name).foreach { expected =>
+      if (UPDATE) {
+        if (actual != expected)
+          writeFile(name, actual)
+      } else {
+        assert(actual == expected)
+      }
+    }
+  }
+
+  def writeFile(name: String, value: String): Unit = {
+    val url = getClass().getResource("/" + name)
+    new PrintWriter(new File(url.getFile())) {
+      write(value)
+      close()
+    }
+  }
 
   for (name <- testFiles) {
     test("test " + name) {
-      val src = Source.fromResource(name + ".c0").mkString
-      val expected = Source.fromResource(name + ".ir.c0").mkString
-
+      val src = getFile(name + ".c0").get
 
       val Parsed.Success(parsed, _) = Parser.parseProgram(src)
       val errors = new ErrorSink()
@@ -37,10 +76,10 @@ class GraphTransformerTests extends AnyFunSuite {
       assert(resolved.isDefined)
 
       val ir = GraphTransformer.transform(resolved.get)
-      val printed = GraphPrinter.print(ir)
-      assert(printed == expected)
+      assertFile(name + ".ir.c0", GraphPrinter.print(ir))
 
-      IRGraphSilver.toSilver(ir)
+      val silver = IRGraphSilver.toSilver(ir).toString()
+      assertFile(name + ".vpr", silver.toString())
     }
   }
 }
