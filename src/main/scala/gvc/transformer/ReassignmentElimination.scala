1c2c642931ad9c286bfcfcd74a20c23df584cf9f
Conrad Zimmerman <me@conradz.com>
2022-01-03 18:15:39 -0500

    Factor Replacer out of ReassignmentElimination

--- src/main/scala/gvc/transformer/ReassignmentElimination.scala
+++ src/main/scala/gvc/transformer/ReassignmentElimination.scala
@@ -9,19 +9,16 @@ import gvc.transformer.IRGraph._
 // (if) and loops (while).
 object ReassignmentElimination {
   def transform(method: Method): Unit = {
-    val transformation = new ReassignmentElimination(method)
-    val reassign = new transformation.Reassignments(Map.empty, ListMap.empty, ListMap.empty)
-
-    method.body.foreach(transformation.replace(_, reassign))
+    val reassign = new Reassignments(method)
+    method.body.foreach(replace(_, reassign))
   }
-}
 
-class ReassignmentElimination(method: Method) {
-  class Reassignments(
-    var mappings: Map[Var, Var],
-    var created: ListMap[Var, Var],
-    var remaining: ListMap[Var, Var]) {
-
+  private class Reassignments(
+    method: Method,
+    var mappings: Map[Var, Var] = Map.empty,
+    var created: ListMap[Var, Var] = ListMap.empty,
+    var remaining: ListMap[Var, Var] = ListMap.empty,
+  ) {
     def reassign(v: Var): Var = {
       val newV = remaining.get(v) match {
         case Some(newV) => {
@@ -45,64 +42,34 @@ class ReassignmentElimination(method: Method) {
           newV
         })
         .getOrElse(v)
+
+    def copy(created: ListMap[Var, Var] = ListMap.empty, remaining: ListMap[Var, Var] = ListMap.empty) =
+      new Reassignments(method = method, mappings = mappings, created = created, remaining = remaining)
   }
 
-  def replace(op: Op, reassign: Reassignments): Unit = {
+  private def replace(op: Op, reassign: Reassignments): Unit = {
+    Replacer.replaceShallow(op, reassign.mappings)
+
     op match {
       case inv: Invoke => {
-        inv.arguments = inv.arguments.map(replace(_, reassign))
-        inv.target = inv.target.map(replace(_, reassign))
-
         inv.target = inv.target.map {
           case v: Var if inv.arguments.exists(_.contains(v)) =>
             reassign.reassign(v)
           case other => other
         }
       }
-      case alloc: AllocValue => {
-        alloc.target = replace(alloc.target, reassign)
-      }
-      case alloc: AllocStruct => {
-        alloc.target = replace(alloc.target, reassign)
-      }
-      case alloc: AllocArray => {
-        //TODO: alloc.length
-        alloc.target = replace(alloc.target, reassign)
-      }
-      case assign: Assign => {
-        assign.target = replace(assign.target, reassign)
-        assign.value = replace(assign.value, reassign)
 
+      case assign: Assign => {
         // Attempt to minimize the number of "patched" assignments by using
         // the variable from the remaining list if one exists
         assign.target = reassign.assign(assign.target)
       }
-      case assign: AssignMember => {
-        assign.value = replace(assign.value, reassign)
-        assign.member = replace(assign.member, reassign)
-      }
-      case assert: Assert => {
-        assert.value = replace(assert.value, reassign)
-      }
-      case fold: Fold => {
-        fold.instance = replace(fold.instance, reassign)
-      }
-      case unfold: Unfold => {
-        unfold.instance = replace(unfold.instance, reassign)
-      }
-      case error: Error => {
-        error.value = replace(error.value, reassign)
-      }
-      case ret: Return => {
-        ret.value = ret.value.map(replace(_, reassign))
-      }
-      case iff: If => {
-        iff.condition = replace(iff.condition, reassign)
 
-        val trueReassign = new Reassignments(reassign.mappings, ListMap(), reassign.remaining)
+      case iff: If => {
+        val trueReassign = reassign.copy(remaining = reassign.remaining)
         iff.ifTrue.foreach(replace(_, trueReassign))
 
-        val falseReassign = new Reassignments(reassign.mappings, ListMap(), reassign.remaining ++ trueReassign.created)
+        val falseReassign = reassign.copy(remaining = reassign.remaining ++ trueReassign.created)
         iff.ifFalse.foreach(replace(_, falseReassign))
 
         // Any mappings created in the false branch and not in the true branch
@@ -118,45 +85,21 @@ class ReassignmentElimination(method: Method) {
         reassign.remaining = reassign.remaining -- created.keys
         reassign.mappings = reassign.mappings ++ created
       }
-      case loop: While => {
-        loop.condition = replace(loop.condition, reassign)
 
+      case loop: While => {
         // Any mappings created inside the loop body are "undone" at the end of the loop
         // body, thus code after the loop body (the conditional, following code, or the
         // next loop iteration) is unaffected.
 
-        val bodyReassign = new Reassignments(reassign.mappings, ListMap.empty, ListMap.empty)
+        val bodyReassign = reassign.copy()
         loop.body.foreach(replace(_, bodyReassign))
 
         bodyReassign.created.foreach {
           case (oldV, newV) => loop.body += new Assign(oldV, newV)
         }
       }
+
+      case _ => ()
     }
   }
-
-  def replace(v: Var, reassign: Reassignments): Var = reassign.mappings.getOrElse(v, v)
-
-  def replace(member: Member, reassign: Reassignments): Member = member match {
-    case field: FieldMember => new FieldMember(replace(field.root, reassign), field.field)
-    case deref: DereferenceMember => new DereferenceMember(replace(deref.root, reassign), deref.valueType)
-    case array: ArrayMember => new ArrayMember(replace(array.root, reassign), array.valueType, array.index) // TODO: index
-  }
-
-  def replace(pred: PredicateInstance, reassign: Reassignments): PredicateInstance =
-    new PredicateInstance(pred.predicate, pred.arguments.map(replace(_, reassign)))
-
-  def replace(expr: Expression, reassign: Reassignments): Expression = expr match {
-    case v: Var => replace(v, reassign)
-    case member: Member => replace(member, reassign)
-    case acc: Accessibility => new Accessibility(replace(acc.member, reassign))
-    case pred: PredicateInstance => replace(pred, reassign)
-    case result: Result => result
-    case imprecise: Imprecise => new Imprecise(imprecise.precise.map(replace(_, reassign)))
-    case literal: Literal => literal
-    case cond: Conditional => new Conditional(replace(cond.condition, reassign), replace(cond.ifTrue, reassign), replace(cond.ifFalse, reassign))
-    case binary: Binary => new Binary(binary.operator, replace(binary.left, reassign), replace(binary.right, reassign))
-    case unary: Unary => new Unary(unary.operator, replace(unary.operand, reassign))
-  }
-
 }
