0b4649eb49614658f25106a05f1e5432e2ddea4d
Conrad Zimmerman <me@conradz.com>
2022-03-28 23:46:27 -0400

    Revamp test infrastructure to run in parallel and execute CLI directly for integration testing

---
+++ src/test/scala/TestUtils.scala
@@ -0,0 +1,138 @@
+package gvc.specs
+
+import java.nio.file._
+import scala.collection.mutable.ListBuffer
+import java.io.IOException
+import gvc.parser.Parser
+import fastparse.Parsed
+import gvc.analyzer._
+import gvc.transformer._
+import gvc.weaver.Weaver
+
+case class TestProgram(
+  ir: IRGraph.Program,
+  silverProgram: SilverProgram,
+) {
+  def weave = Weaver.weave(ir, silverProgram)
+
+  def irSource = GraphPrinter.print(ir, true)
+
+  def silverSource = silverProgram.program.toString()
+}
+
+case class TestResource(file: Path) {
+  def read(): String =
+    Files.readString(file)
+
+  def update(contents: String) = {
+    val relativePath = TestUtils.resourcesRoot.relativize(file.toAbsolutePath())
+    val resourcePath = TestUtils.resourcesSourceRoot.resolve(relativePath)
+    Files.writeString(resourcePath, contents)
+  }
+}
+
+// This encapsulates the pattern of grouping input and expected output
+// files. Files such as `input.c`, `input.output.c`, `input.txt` will all
+// be grouped together so that a single test can process the group of files.
+// Note that the name is assumed to be relative to the `resourcesRoot`.
+class FileCollection(val name: String) {
+  // ID that can be used for filenames
+  val id = name.replace(FileSystems.getDefault.getSeparator(), "_")
+
+  def apply(ext: String): TestResource =
+    get(ext).getOrElse(throw new Exception(s"Could not find '${name + ext}'"))
+
+  def get(ext: String): Option[TestResource] = {
+    val file = TestUtils.resourcesRoot.resolve(name + ext)
+    if (Files.exists(file))
+      Some(TestResource(file))
+    else
+      None
+  }
+}
+
+class ProgramException(message: String) extends Exception(message)
+class ParserException(message: String) extends ProgramException(message)
+class ResolverException(message: String) extends ProgramException(message)
+class TypeCheckerException(message: String) extends ProgramException(message)
+class ValidatorException(message: String) extends ProgramException(message)
+
+object TestUtils {
+  val resourcesRoot = Paths.get(getClass.getResource("/").toURI())
+  val resourcesSourceRoot = Paths.get("src/test/resources")
+
+  def getAllFiles(dir: Path): List[Path] = {
+    val files = ListBuffer[Path]()
+    Files.walkFileTree(dir.toAbsolutePath(), new SimpleFileVisitor[Path] {
+      override def visitFile(file: Path, attrs: attribute.BasicFileAttributes): FileVisitResult = {
+        files += file.toAbsolutePath()
+        FileVisitResult.CONTINUE
+      }
+    })
+    files.toList
+  }
+
+  // This basename operates differently to normal basename:
+  // It does not remove the directory
+  // It strips multiple "extensions", i.e. `root.a.b.c` -> `root`
+  def basename(file: Path): String = {
+    file.toString().replaceFirst("\\.[^/\\\\]*$", "")
+  }
+
+  // Groups files by their basename (see notes on modified basename)
+  def groupFiles(files: List[Path]): List[FileCollection] =
+    files.groupBy(basename(_))
+      .map { case (base, _) => new FileCollection(base) }
+      .toList
+
+  def groupResources(directory: String): List[FileCollection] = {
+    groupFiles(
+      getAllFiles(resourcesRoot.resolve(directory))
+        .map(f => resourcesRoot.relativize(f)))
+  }
+
+  def deleteDirectory(dir: Path): Unit = {
+    Files.walkFileTree(dir, new SimpleFileVisitor[Path] {
+      override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult = {
+        Files.delete(dir)
+        FileVisitResult.CONTINUE
+      }
+      override def visitFile(file: Path, attrs: attribute.BasicFileAttributes): FileVisitResult = {
+        Files.delete(file)
+        FileVisitResult.CONTINUE
+      }
+    })
+  }
+
+  // Utility for parsing and generating a complete program, both IR and Silver
+  def program(source: String): TestProgram = {
+    val parsed = Parser.parseProgram(source) match {
+      case fail: Parsed.Failure => throw new ParserException(fail.trace().longMsg)
+      case Parsed.Success(p, _) => p
+    }
+
+    val sink = new ErrorSink()
+    def getErrors = sink.errors.map(_.message).mkString("\n")
+
+    val result = Resolver.resolveProgram(parsed, List(), sink)
+    if (!sink.errors.isEmpty)
+      throw new ResolverException(getErrors)
+    
+    TypeChecker.check(result, sink)
+    if (!sink.errors.isEmpty)
+      throw new TypeCheckerException(getErrors)
+
+
+    AssignmentValidator.validate(result, sink)
+    ReturnValidator.validate(result, sink)
+    SpecificationValidator.validate(result, sink)
+    ImplementationValidator.validate(result, sink)
+    if (!sink.errors.isEmpty)
+      throw new ValidatorException(getErrors)
+
+    val ir = GraphTransformer.transform(result)
+    val silver = IRGraphSilver.toSilver(ir)
+
+    TestProgram(ir, silver)
+  }
+}
---
+++ src/test/scala/integration/CompilerSpec.scala
@@ -0,0 +1,69 @@
+package gvc.specs.integration
+
+import org.scalatest.funsuite.AnyFunSuite
+import java.lang.ProcessBuilder.Redirect
+
+import gvc.specs._
+import org.scalatest._
+import java.nio.file._
+import gvc.specs.BaseFileSpec
+import scala.io.Source
+
+class CompilerSpec extends AnyFunSuite with BaseFileSpec with ParallelTestExecution {
+  var output: Path = null
+  val javaArgs = List("-Xss15m")
+
+  override protected def withFixture(test: NoArgTest): Outcome = {
+    try {
+      output = Files.createTempDirectory("gvc0")
+      super.withFixture(test)
+    } finally {
+      TestUtils.deleteDirectory(output)
+    }
+  }
+
+  def compile(args: String*): Unit = {
+    val jvm = ProcessHandle.current().info().command().get()
+    val mainClass = "gvc.Main"
+
+    val command = jvm :: javaArgs ::: "-classpath" :: getClasspath :: mainClass :: args.toList
+    execute(command:_*)
+  }
+
+  def execute(command: String*): Unit = {
+    val proc = new ProcessBuilder(command:_*)
+      .redirectError(Redirect.INHERIT)
+      .redirectOutput(Redirect.PIPE)
+      .start()
+
+    val exit = proc.waitFor()
+    if (exit != 0) {
+      info("Output: " + Source.fromInputStream(proc.getInputStream()).mkString)
+      fail(s"Command '${command.mkString(" ")}' exited with code $exit")
+    }
+  }
+
+  (TestUtils.groupResources("verifier") ++ TestUtils.groupResources("quant-study")).foreach { input =>
+    test(s"test ${input.name}") {
+      val source = input(".c0").read()
+      val id = input.id
+
+      // Copy to temporary directory
+      val sourceFile = output.resolve(id + ".c0")
+      Files.writeString(sourceFile, source)
+
+      val outputExe = output.resolve(id)
+      compile(s"--output=${outputExe}", "--save-files", sourceFile.toString())
+
+      execute(outputExe.toString())
+
+      val verified = Files.readString(output.resolve(id + ".verified.c0"))
+      val ir = Files.readString(output.resolve(id + ".ir.c0"))
+      val vpr = Files.readString(output.resolve(id + ".vpr"))
+
+      assertFile(input.get(".output.c0"), verified)
+      assertFile(input.get(".ir.c0"), ir)
+      assertFile(input.get(".vpr"), vpr)
+    }
+  }
+}
