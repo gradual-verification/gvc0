f0f2905a92f40e95cf109428fd7cd4c104900f84
Ian C. McCormack <icmccorm@andrew.cmu.edu>
2022-05-31 18:29:01 -0400

    Fixed missing path to .jar file in run scripts.

--- study/run_bst.sh
+++ study/run_bst.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xss15M -jar -b ./study/benchmarks/bst/ --only-exec -ws 4 -wu 64 -i 7 ./src/test/resources/quant-study/bst.c0; shutdown -h now
+java -Xss15M -jar target/scala-2.12/gvc0-assembly-0.1.0-SNAPSHOT.jar -b ./study/benchmarks/bst/ --only-exec -ws 4 -wu 64 -i 7 ./src/test/resources/quant-study/bst.c0; shutdown -h now
--- study/run_list.sh
+++ study/run_list.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xss15M -jar -b ./study/benchmarks/list/ --only-exec -ws 8 -wu 128 -i 7 ./src/test/resources/quant-study/list.c0; shutdown -h now
+java -Xss15M -jar target/scala-2.12/gvc0-assembly-0.1.0-SNAPSHOT.jar -b ./study/benchmarks/list/ --only-exec -ws 8 -wu 128 -i 7 ./src/test/resources/quant-study/list.c0; shutdown -h now
