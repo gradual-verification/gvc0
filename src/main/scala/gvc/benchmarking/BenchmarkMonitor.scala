6f25e762570bacede8174d4992615d0dd72a4cf4
Ian McCormack <icmccorm@andrew.cmu.edu>
2022-08-11 10:52:21 -0400

    Removed unnecessary double iteration in monitor and adjusted coloring so that only valid errors will show up as red.

--- src/main/scala/gvc/benchmarking/BenchmarkMonitor.scala
+++ src/main/scala/gvc/benchmarking/BenchmarkMonitor.scala
@@ -1,4 +1,5 @@
 package gvc.benchmarking
+
 import gvc.benchmarking.DAO.DBConnection
 import gvc.benchmarking.Output.{green, red}
 
@@ -39,10 +40,10 @@ object BenchmarkMonitor {
     Output.title("Completed Programs")
     printCompletedPrograms(conn)
 
-    Output.title("Completed Mass Benchmarks")
+    Output.title("Completed Paths")
     printCompletedPaths(conn)
 
-    Output.title("Completed Preconfigured Benchmarks")
+    Output.title("Completed Benchmarks")
     printCompletedBenchmarks(conn)
 
   }
@@ -50,44 +51,39 @@ object BenchmarkMonitor {
   private def printCompletedPrograms(conn: DBConnection): Unit = {
     val completed = DAO.getCompletedProgramList(conn)
     if (completed.nonEmpty) {
-      val grouped = completed
+      completed
         .groupBy(_.versionName)
-        .map(g1 => {
-          g1._1 -> g1._2
+        .foreach(g1 => {
+          println(s"* V: ${g1._1}")
+          g1._2
             .groupBy(_.hwName)
-            .map(g2 => {
-              g2._1 -> g2._2
+            .foreach(g2 => {
+              println(s"\t* HW: ${g2._1}")
+              g2._2
                 .groupBy(_.stress)
-                .map(g3 => {
-                  g3._1 -> g3._2
+                .foreach(g3 => {
+                  println(s"\t\t* w = ${g3._1}")
+                  g3._2
                     .groupBy(_.measurementType)
-                    .map(g4 => {
-                      g4._1 -> g4._2
+                    .foreach(g4 => {
+                      val elem = g4._2.head
+                      val completionPercentage = Math.round(
+                        ((elem.completed.toDouble / elem.total) * 100) * 100) / 100
+                      val errorPercentage = Math.round(
+                        ((elem.errored.toDouble / elem.total) * 100) * 100) / 100
+                      val errorColoring: String => String =
+                        if (elem.errored == 0) green else red
+                      println(s"\t\t\t* ${g4._1}: ${
+                        green(
+                          completionPercentage.toString + "%")
+                      }, (${elem.completed} total) - ${
+                        errorColoring(
+                          errorPercentage.toString + "%")
+                      }, (${elem.errored} total)")
                     })
                 })
             })
         })
-      grouped.keySet.foreach(k => {
-        println(s"* V: $k")
-        grouped(k).foreach(p => {
-          println(s"\t* HW: ${p._1}")
-          val m = p._2
-          m.foreach(p1 => {
-            println(s"\t\t* w = ${p1._1}")
-            val m2 = p1._2
-            m2.foreach(p2 => {
-              val elem = p2._2.head
-              val completionPercentage = Math.round(
-                ((elem.completed.toDouble / elem.total) * 100) * 100) / 100
-              val errorPercentage = Math.round(
-                ((elem.errored.toDouble / elem.total) * 100) * 100) / 100
-              println(s"\t\t\t* ${p2._1}: ${green(
-                completionPercentage.toString + "%")}, (${elem.completed} total) - ${red(
-                errorPercentage.toString + "%")}, (${elem.errored} total)")
-            })
-          })
-        })
-      })
     } else {
       println("N/A\n")
     }
@@ -96,45 +92,32 @@ object BenchmarkMonitor {
   private def printCompletedPaths(conn: DBConnection): Unit = {
     val completed = DAO.getCompletedPathList(conn)
     if (completed.nonEmpty) {
-      val grouped = completed
+      completed
         .groupBy(_.version)
-        .map(g1 => {
-          g1._1 -> g1._2
+        .foreach(g1 => {
+          println(s"* V: ${g1._1}")
+          g1._2
             .groupBy(_.hardware)
-            .map(g2 => {
-              g2._1 -> g2._2
+            .foreach(g2 => {
+              println(s"\t* HW: ${g2._1}")
+              g2._2
                 .groupBy(_.src_filename)
-                .map(g3 => {
-                  g3._1 -> g3._2
+                .foreach(g3 => {
+                  println(s"\t\t* Program: ${g3._1}")
+                  g3._2
                     .groupBy(_.workload)
-                    .map(g4 => {
-                      g4._1 -> g4._2
+                    .foreach(g4 => {
+                      println(s"\t\t\t* w = ${g4._1}")
+                      g4._2
                         .groupBy(_.measurementMode)
-                        .map(g5 => {
-                          g5._1 -> g5._2
+                        .foreach(g5 => {
+                          println(
+                            s"\t\t\t\t* ${g5._1}: ${g5._2.head.num_paths}")
                         })
                     })
                 })
             })
         })
-      grouped.keySet.foreach(k => {
-        println(s"* V: $k")
-        grouped(k).foreach(p => {
-          println(s"\t* HW: ${p._1}")
-          val m = p._2
-          m.foreach(p1 => {
-            println(s"\t\t* Program: ${p1._1}")
-            val m2 = p1._2
-            m2.foreach(p2 => {
-              println(s"\t\t\t* w = ${p2._1}")
-              val m3 = p2._2
-              m3.foreach(p3 => {
-                println(s"\t\t\t\t* ${p3._1}: ${p3._2.head.num_paths}")
-              })
-            })
-          })
-        })
-      })
     } else {
       println("N/A\n")
     }
@@ -143,36 +126,27 @@ object BenchmarkMonitor {
   private def printCompletedBenchmarks(conn: DBConnection): Unit = {
     val completed = DAO.getCompletedBenchmarks(conn)
     if (completed.nonEmpty) {
-      val grouped = completed
+      completed
         .groupBy(_.benchmark)
-        .map(
-          c1 =>
-            c1._1 -> c1._2
-              .groupBy(_.version)
-              .map(c2 =>
-                c2._1 -> c2._2
-                  .groupBy(_.hardware)
-                  .map(c3 => {
-                    c3._1 -> c3._2
-                      .groupBy(_.measurementMode)
-                      .map(c4 => {
-                        c4._1 -> c4._2
-                      })
-                  })))
-
-      grouped.keySet.foreach(s => {
-        println(s"* B: $s")
-        grouped(s).foreach(p => {
-          println(s"\t* V: ${p._1}")
-          p._2.foreach(p1 => {
-            println(s"\t\t* HW: ${p1._1}")
-            p1._2.foreach(p2 => {
-              println(
-                s"\t\t\t* ${p2._1}: (${p2._2.map(_.stress).mkString(",")})")
+        .foreach(c1 => {
+          println(s"* B: ${c1._1}")
+          c1._2
+            .groupBy(_.version)
+            .foreach(c2 => {
+              println(s"\t* V: ${c2._1}")
+              c2._2
+                .groupBy(_.hardware)
+                .foreach(c3 => {
+                  println(s"\t\t* HW: ${c3._1}")
+                  c3._2
+                    .groupBy(_.measurementMode)
+                    .foreach(c4 => {
+                      println(
+                        s"\t\t\t* ${c4._1}: (${c4._2.map(_.stress).mkString(",")})")
+                    })
+                })
             })
-          })
         })
-      })
     } else {
       println("N/A\n")
     }
