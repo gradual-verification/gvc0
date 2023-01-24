a4f891b8b5c7f67124f9c53887ea461122d727db
Ian McCormack <icmccorm@andrew.cmu.edu>
2023-01-24 17:29:22 -0500

    Ensured that gmon.sum is copied to profiling data directory.

--- src/main/scala/gvc/benchmarking/GProf.scala
+++ src/main/scala/gvc/benchmarking/GProf.scala
@@ -3,12 +3,13 @@ package gvc.benchmarking
 import gvc.Config
 import gvc.Config.error
 
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Files, Path, Paths, StandardCopyOption}
 import scala.sys.process.Process
 
 class GProf(binary: Path, destination: Path) {
   private val gprof_exe = Config.resolveToolPath("gprof", "GPROF_EXE")
   private val profilingOutput = Paths.get("./gmon.out")
+  private val profilingSum = Paths.get("./gmon.sum")
 
   def merge: Unit = {
     if (Files.exists(destination)) {
@@ -18,6 +19,10 @@ class GProf(binary: Path, destination: Path) {
       val exit = commandAsProcess.run().exitValue()
       if (exit != 0) {
         error(s"Failed to merge gprof output to ${destination.toString}")
+      } else {
+        Files.copy(profilingSum,
+                   destination,
+                   StandardCopyOption.REPLACE_EXISTING)
       }
     } else {
       Files.copy(profilingOutput, destination)
